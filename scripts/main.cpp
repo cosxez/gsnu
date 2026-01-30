03b763699c14cb9aa27090677cce609f1ae683fa
cosxez <faceless113x@gmail.com>
2026-01-30 15:31:12 +0400

    Add files via upload

--- scripts/main.cpp
+++ scripts/main.cpp
@@ -13,12 +13,13 @@ using namespace std;
 
 int main(int args, char* argv[])
 {
-	SDL_Init(NULL);
+	SDL_Init(SDL_INIT_VIDEO);
 
 	SDL_Window* win = SDL_CreateWindow("Viewer", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 1024, 940, SDL_WINDOW_SHOWN);
 
 	SDL_Renderer* ren = SDL_CreateRenderer(win, -1, 0);
-
+	
+	bool run=true;
 	SDL_Event event;
 
 	Model saya;
@@ -30,22 +31,27 @@ int main(int args, char* argv[])
 
 	SDL_GetWindowSize(win, &width, &height);
 	
-	while (true)
+	while (run)
 	{
-		SDL_PollEvent(&event);
-
-		if (event.type == SDL_QUIT)
+		while (SDL_PollEvent(&event))
 		{
-			break;
+			if (event.type == SDL_QUIT)
+			{
+				run=false;
+			}
 		}
-
+		
 		SDL_SetRenderDrawColor(ren, 0, 0, 0, 255);
 		SDL_RenderClear(ren);
 		SDL_SetRenderDrawColor(ren, 255, 255, 255, 255);
 		
 		if (scene == 0x00)
 		{
-			render3(win,ren,saya,0x01,135,width/2+(width/100*20), 0, 1.3);
+			//wall
+			triangle3(win,ren,width/2-10,200,0,width/2-10,-200,0,150,-200,2);
+			triangle3(win,ren,width/2-10,200,0,150,-200,2,150,200,2);
+				
+			render3(win,ren,saya,0x01,125,width/2+(width/100*20), 0, 1.3);
 		}
 		
 		SDL_RenderPresent(ren);
